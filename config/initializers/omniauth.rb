ab905b32b82e82ef1feac1aa6bba26650199a76e
Chris Continanza <christopher.continanza@gmail.com>
2011-08-26 14:24:01 -0700

    read auth from URL params

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -14,9 +14,7 @@ if Rails.env.production?
   OmniAuth.config.full_host = 'https://dbdotcom-demo--c.herokuapp.com'
 end
 
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :salesforce, 
-    '3MVG9lKcPoNINVBK_LnqNBBqffM8vYaBk4C7o8nv3WFzocxGFH7PsvuVRL.zLq_XQceVq80gw_bUr2UtUkrKY', 
-    '5304485318827063310'
+  url = ENV['DATABASE_COM_URL']
+  provider :salesforce, url.params['oauth_secret'], url.params['oauth_secret']
 end
