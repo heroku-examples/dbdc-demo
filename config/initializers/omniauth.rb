d4899b565102f2038a8c5a9758e18dc81f086cf2
Chris Continanza <christopher.continanza@gmail.com>
2011-08-26 13:15:37 -0700

    update secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,11 +10,13 @@ class OmniAuth::Strategies::Salesforce
   end
 end 
 
-OmniAuth.config.full_host = 'https://dbdotcom-demo--c.herokuapp.com'
+if Rails.env.production? 
+  OmniAuth.config.full_host = 'https://dbdotcom-demo--c.herokuapp.com'
+end
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :salesforce, 
-    '3MVG9lKcPoNINVBIjB.wiPN0cNZc8CjYEv..laOBgpzAGHIzt3H8PQPkhN_bH11.KLN3LlOqBDniNJrd5emRi', 
-    '1038986679638308795'
+    '3MVG9lKcPoNINVBK_LnqNBBqffM8vYaBk4C7o8nv3WFzocxGFH7PsvuVRL.zLq_XQceVq80gw_bUr2UtUkrKY', 
+    '5304485318827063310'
 end
