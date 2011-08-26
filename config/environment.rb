2f036ba35c26c6c8312bfddb184f2ded2014f130
Chris Continanza <christopher.continanza@gmail.com>
2011-08-26 08:57:15 -0700

    sync for logs

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 load File.expand_path('../../keys.rb', __FILE__) if Rails.env.development?
 
+STDOUT.sync = STDERR.sync = true
+
 # Initialize the rails application
 DbComDemo::Application.initialize!
