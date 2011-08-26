d59729d125e4a32e1a07477ed05d89cfe5995a4a
Chris Continanza <christopher.continanza@gmail.com>
2011-08-25 19:25:26 -0700

    load keys.rb only in dev

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-load File.expand_path('../../keys.rb', __FILE__)
+load File.expand_path('../../keys.rb', __FILE__) if Rails.env.development?
 
 # Initialize the rails application
 DbComDemo::Application.initialize!
