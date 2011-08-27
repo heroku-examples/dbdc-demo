50211c07437337fe335561bafe83aee4aa4cecf8
Glenn Gillen <me@glenngillen.com>
2011-08-26 17:29:34 -0700

    Force establish connection.

--- app/models/s3_uploader.rb
+++ app/models/s3_uploader.rb
@@ -1,5 +1,6 @@
 class S3Uploader
   def self.upload(filename, data)
+    establish_connection!
     AWS::S3::S3Object.store(filename, data, bucket_name,
                             :access => :public_read)
     AWS::S3::S3Object.url_for(filename, bucket_name,
@@ -10,6 +11,13 @@ class S3Uploader
   private
   def self.bucket_name
     "dreamforce11.dbcomdemo"
+  end
+
+  def self.establish_connection!
+    AWS::S3::Base.establish_connection!(
+      :access_key_id     => ENV["AWS_KEY"] || "aws-key",
+      :secret_access_key => ENV["AWS_SECRET"] || "aws-secret"
+    )
   end
 
 end
--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -1,5 +1,5 @@
-# AWS::S3::Base.establish_connection!(
-#   :access_key_id     => ENV["AWS_KEY"] || "aws-key",
-#   :secret_access_key => ENV["AWS_SECRET"] || "aws-secret"
-# )
+AWS::S3::Base.establish_connection!(
+  :access_key_id     => ENV["AWS_KEY"] || "aws-key",
+  :secret_access_key => ENV["AWS_SECRET"] || "aws-secret"
+)
 
